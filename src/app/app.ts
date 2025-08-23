74ed5d09eb06a946e4e83e3d9bc95735520f1698
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-08-23 22:49:52 +0530

    first programme

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('firstp');
+  protected readonly title = signal('Awishka');
 }
